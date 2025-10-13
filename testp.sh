665d4737d3c77954cd22290b00b6f76d7f2ab7f3
Andtherya <Andy17998@outlook.com>
2025-10-13 13:20:26 +0800

    Update testp.sh

--- testp.sh
+++ testp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ALL_PROXY="socks5://socks5:socks5@38.47.125.251:5555"
+export ALL_PROXY="socks5://127.0.0.1:28081"
 
 UUID="${UUID:-5861ed67-f4ae-4e02-868e-9cea7d2d5a9e}"
 ARGO_DOMAIN="${ARGO_DOMAIN:-}"
@@ -10,6 +10,7 @@ CFIP="${CFIP:-www.visa.com.sg}"
 CFPORT="${CFPORT:-443}"
 NAME="${NAME:-Vls}"
 
+curl -4 ifconfig.me
 pkill bot
 pkill web
 
