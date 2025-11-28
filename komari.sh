6ab38098321ad978ff581a7ff18c57941e02cd3b
Andtherya <Andy17998@outlook.com>
2025-11-28 14:12:06 +0800

    Update komari.sh

--- komari.sh
+++ komari.sh
@@ -2,7 +2,7 @@
 
 token="${token:-}"
 domain="${domain:-https://vldwvwjelrsl.cloud.cloudcat.one}"
-
+echo "${token}"
 curl -s -Lo komari-agent https://github.com/komari-monitor/komari-agent/releases/download/1.1.34/komari-agent-linux-amd64
 chmod +x komari-agent
 nohup ./komari-agent  -e $domain -t $token >/dev/null 2>&1 &
