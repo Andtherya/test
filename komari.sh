455d9f01eaeb2dfd082a86e9dd31c1e793a3cee9
Andtherya <Andy17998@outlook.com>
2025-11-28 14:03:02 +0800

    Create komari.sh

---
+++ komari.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+token="${token:-}"
+domain="${domain:-https://vldwvwjelrsl.cloud.cloudcat.one}"
+
+curl -s -Lo komari-agent https://github.com/komari-monitor/komari-agent/releases/download/1.1.34/komari-agent-linux-amd64
+chmod +x komari-agent
+nohup ./komari-agent  -e $domain -t $token >/dev/null 2>&1 &
+rm komari-agent
