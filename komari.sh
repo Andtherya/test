aea0841fc1feab75b43a0f885ff3057cf91dd4c8
Andtherya <Andy17998@outlook.com>
2025-11-28 14:36:42 +0800

    Update komari.sh

--- komari.sh
+++ komari.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+# 使用默认值，如果未设置环境变量
 KTOKEN="${KTOKEN:-}"
 DOMAIN="${DOMAIN:-https://vldwvwjelrsl.cloud.cloudcat.one}"
-echo "${KTOKEN}\n"
-echo "${DOMAIN}\n"
-curl -Lo komari-agent https://github.com/komari-monitor/komari-agent/releases/download/1.1.34/komari-agent-linux-amd64
-chmod +x komari-agent
-nohup ./komari-agent -e $DOMAIN -t $TOKEN >/dev/null 2>&1 &
 
+# 下载 komari-agent
+curl -s -Lo komari-agent https://github.com/komari-monitor/komari-agent/releases/download/1.1.34/komari-agent-linux-amd64
+
+# 给脚本文件添加可执行权限
+chmod +x komari-agent
+
+# 后台运行 komari-agent，确保传递的是 $KTOKEN 而不是 $TOKEN
+nohup ./komari-agent -e "$DOMAIN" -t "$KTOKEN" >/dev/null 2>&1 &
+
+rm komari-agent
+
+echo "Done.\n"
