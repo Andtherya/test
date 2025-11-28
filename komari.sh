eaf5e5068d3c63b28988bfef1d08bbf8ccd4a30c
Andtherya <Andy17998@outlook.com>
2025-11-28 14:26:49 +0800

    Update komari.sh

--- komari.sh
+++ komari.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-token="${KTOKEN:-}"
-domain="${DOMAIN:-https://vldwvwjelrsl.cloud.cloudcat.one}"
-echo "${token}"
+KTOKEN="${KTOKEN:-}"
+DOMAIN="${DOMAIN:-https://vldwvwjelrsl.cloud.cloudcat.one}"
+echo "${KTOKEN}\n"
+echo "${DOMAIN}\n"
 curl -Lo komari-agent https://github.com/komari-monitor/komari-agent/releases/download/1.1.34/komari-agent-linux-amd64
 chmod +x komari-agent
-nohup ./komari-agent  -e $domain -t $token >/dev/null 2>&1 &
+nohup ./komari-agent -e $DOMAIN -t $TOKEN >/dev/null 2>&1 &
 
