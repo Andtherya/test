b31ffc10a59dc133e5ea874487d06523bed9afcd
Andtherya <Andy17998@outlook.com>
2025-12-04 16:18:01 +0800

    Update sb-vps.sh

--- sb-vps.sh
+++ sb-vps.sh
@@ -140,6 +140,8 @@ eQ6OFb9LbLYL9f+sAiAffoMbi4y/0YUSlTtz7as9S8/lciBF5VCUoVIKS+vX2g==
 EOF
 fi
 
+workspace=$(pwd)
+
   cat > config.json << EOF
 {
     "log": {
@@ -181,8 +183,8 @@ fi
         "alpn": [
           "h3"
         ],
-        "certificate_path": "'${FILE_PATH}'/cert.pem",
-        "key_path": "'${FILE_PATH}'/private.key"
+        "certificate_path": "'${workspace}'/cert.pem",
+        "key_path": "'${workspace}'/private.key"
       }
     }'; fi)$(if [ "$HY2_PORT" != "" ]; then echo ',
     {
@@ -201,8 +203,8 @@ fi
             "alpn": [
                 "h3"
             ],
-            "certificate_path": "'${FILE_PATH}'/cert.pem",
-            "key_path": "'${FILE_PATH}'/private.key"
+            "certificate_path": "'${workspace}'/cert.pem",
+            "key_path": "'${workspace}'/private.key"
           }
       }'; fi)$(if [ "$REALITY_PORT" != "" ]; then echo ',
       {
