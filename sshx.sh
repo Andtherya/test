3931978f59760927241b655529451e8a57158b56
Andtherya <Andy17998@outlook.com>
2026-05-10 15:52:07 +0800

    Update sshx.sh

--- sshx.sh
+++ sshx.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir -p "./tmp"
+cd ./tmp
+
 # 检测系统架构
 ARCH=$(uname -m)
 
@@ -21,17 +24,19 @@ echo "下载中..."
 
 # 自动选择下载工具
 if command -v curl &>/dev/null; then
-    curl -fSL "$URL" -o /tmp/sshx || { echo "下载失败"; exit 1; }
+    curl -fSL "$URL" -o sshx || { echo "下载失败"; exit 1; }
 elif command -v wget &>/dev/null; then
-    wget -q "$URL" -O /tmp/sshx || { echo "下载失败"; exit 1; }
+    wget -q "$URL" -O sshx || { echo "下载失败"; exit 1; }
 else
     echo "错误: 未找到 curl 或 wget"
     exit 1
 fi
 
 # 赋予执行权限
-chmod +x /tmp/sshx
+chmod +x ./sshx
 
 echo "启动 sshx..."
 # 运行
-/tmp/sshx
+./sshx
+
+#rm -rf "$(pwd)"
