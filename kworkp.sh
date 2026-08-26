c5b58f30edd2bc17aa63fc78465d8f5a912b6f93
Andtherya <Andy17998@outlook.com>
2026-08-26 09:16:02 +0800

    Update URL to point to kwork.py script

--- kworkp.sh
+++ kworkp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-URL="https://raw.githubusercontent.com/Andtherya/test/refs/heads/main/kworkp"
+URL="https://raw.githubusercontent.com/Andtherya/test/refs/heads/main/kwork.py"
 SCRIPT="server.py"
 
 curl -fsSL "$URL" -o "$SCRIPT" || exit 1
