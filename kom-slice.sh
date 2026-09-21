f8024cd23fb1561e74523cffcd489b81990a0252
Andtherya <Andy17998@outlook.com>
2026-09-21 16:32:52 +0800

    Update kom-slice.sh

--- kom-slice.sh
+++ kom-slice.sh
@@ -6,7 +6,7 @@ export AGENT_AUTO_DISCOVERY_KEY="${AGENT_AUTO_DISCOVERY_KEY:-}"
 export AGENT_TOKEN="${AGENT_TOKEN:-}"
 export AGENT_ENDPOINT="${AGENT_ENDPOINT:-}"
 export AGENT_DISABLE_AUTO_UPDATE="${AGENT_DISABLE_AUTO_UPDATE:-true}"
-WORKDIR="${WORKDIR:-$pwd/test}"
+
 
 VERSION="1.1.38"
 BASE_URL="https://github.com/komari-monitor/komari-agent/releases/download/${VERSION}"
@@ -21,8 +21,6 @@ case "$arch" in
     *) echo "不支持的架构: $arch" && exit 1 ;;
 esac
 
-# 下载
-mkdir -p "${WORKDIR}" && cd "${WORKDIR}"
 [ ! -f "bot" ] && {
     if command -v curl &>/dev/null; then
         curl -fsSL -o bot "${BASE_URL}/komari-agent-linux-${arch}"
@@ -33,8 +31,8 @@ mkdir -p "${WORKDIR}" && cd "${WORKDIR}"
 }
 
 # 停止旧进程并启动
-pkill -f "${WORKDIR}/bot" 2>/dev/null || true
+pkill -f "$pwd/bot" 2>/dev/null || true
 sleep 1
-nohup "${WORKDIR}/bot" >/dev/null 2>&1 &
+nohup "$pwd/bot" >/dev/null 2>&1 &
 sleep 1
-rm -rf "${WORKDIR}"
+rm  "$pwd/bot"
